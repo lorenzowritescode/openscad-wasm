c0bec0d72754acfb45b02f8539f5039f92674585
Lorenzo Paoliani <5505342+lorenzowritescode@users.noreply.github.com>
2025-01-22 22:11:33 +0000

    replace evil function that breaks next build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
 import typescript from "@rollup/plugin-typescript";
 import { wasm } from "@rollup/plugin-wasm";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const traverse = require("@babel/traverse").default;
+const generate = require("@babel/generator").default;
+const { parse } = require("@babel/parser");
 
 export default {
   input: `src/openscad.ts`,
@@ -12,5 +18,31 @@ export default {
       targetEnv: "auto-inline",
       maxFileSize: 0,
     }),
+    {
+      name: "patch-findWasmBinary-function",
+      renderChunk(code) {
+        const replacementFunctionCode = `
+        function findWasmBinary() {
+          return;
+        }
+        `;
+
+        const replacementAst = parse(replacementFunctionCode, {
+          sourceType: "module",
+        }).program.body[0];
+        const ast = parse(code, { sourceType: "module" });
+
+        traverse(ast, {
+          FunctionDeclaration(path) {
+            if (path.node.id?.name === "findWasmBinary") {
+              path.replaceWith(replacementAst);
+            }
+          },
+        });
+
+        const { code: transformedCode } = generate(ast);
+        return transformedCode;
+      },
+    },
   ],
 };
